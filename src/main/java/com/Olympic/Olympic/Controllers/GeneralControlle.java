5670bab9eb2a70696cad61092c3cfdc66b5953f2
dhikra909 <alm@ka909@gmail.com>
2023-05-21 15:58:54 +0400

    25

--- src/main/java/com/Olympic/Olympic/Controllers/GeneralControlle.java
+++ src/main/java/com/Olympic/Olympic/Controllers/GeneralControlle.java
@@ -10,7 +10,7 @@ public class GeneralControlle {
 
 //    SlackClient slackClient;
 
-    @GetMapping(value = "test")
+    
     @PreAuthorize("hasRole('USER')")
     public String test(){
         return "${spring.profiles.active}";

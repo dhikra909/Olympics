53ba2239c3d46fbb424f638b02f65e46f9b317e6
dhikra909 <alm@ka909@gmail.com>
2023-05-21 16:20:19 +0400

    46

--- src/main/java/com/Olympic/Olympic/Controllers/MedalStandingsConteoler.java
+++ src/main/java/com/Olympic/Olympic/Controllers/MedalStandingsConteoler.java
@@ -42,7 +42,10 @@ public class MedalStandingsConteoler {
         medalStandingsServices.deleteMedalStandingsById(id);
     }
 
-  
+    @GetMapping(value = "/getAllMedalStandings")
+    public List<MedalStandings> getAllEvent() {
+        return medalStandingsServices.getAllMedalStandings();
+    }
 
 
 

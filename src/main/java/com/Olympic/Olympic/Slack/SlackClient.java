a078df122c3a4796a0817af3c386857c20e6b55a
dhikra909 <alm@ka909@gmail.com>
2023-05-22 09:19:20 +0400

    Slack

---
+++ src/main/java/com/Olympic/Olympic/Slack/SlackClient.java
@@ -0,0 +1,15 @@
+package com.Olympic.Olympic.Slack;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class SlackClient {
+
+    public String sendMessage(String text){
+        return WebClient.create().post()
+                .uri("https://hooks.slack.com/services/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")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new SlackPayload(text))
+                .retrieve().bodyToMono(String.class)
+                .block();    }
+}

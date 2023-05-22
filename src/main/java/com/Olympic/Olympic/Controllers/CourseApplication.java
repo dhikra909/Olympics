90db8a6cc00fa354d88950f6297ced9b5ad09c71
dhikra909 <alm@ka909@gmail.com>
2023-05-22 12:49:34 +0400

    security code

--- src/main/java/com/Olympic/Olympic/Controllers/CourseApplication.java
+++ src/main/java/com/Olympic/Olympic/Controllers/CourseApplication.java
@@ -1,13 +1,15 @@
-//package com.Olympic.Olympic.Controllers;
-//
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.scheduling.annotation.EnableScheduling;
-////import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-////import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//
-//@SpringBootApplication
-//@EnableScheduling
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
-//public class CourseApplication {
-//}
+package com.Olympic.Olympic.Controllers;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class CourseApplication {
+}
--- src/main/java/com/Olympic/Olympic/Controllers/GeneralControlle.java
+++ src/main/java/com/Olympic/Olympic/Controllers/GeneralControlle.java
@@ -1,25 +1,25 @@
-//package com.Olympic.Olympic.Controllers;
-//
-//import org.springframework.security.access.prepost.PreAuthorize;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//
-//import static java.awt.SystemColor.text;
-//
-//public class GeneralControlle {
-//
-////    SlackClient slackClient;
-//
-//    @GetMapping(value = "test")
-//    @PreAuthorize("hasRole('USER')")
-//    public String test(){
-//        return "${spring.profiles.active}";
-//    }
-//
-//
-//    @GetMapping("/admin")
-//    @PreAuthorize("hasRole('ADMIN')")
-//    public String adminOnly() {
-//        return "You are an admin!";
-//    }
-//}
+package com.Olympic.Olympic.Controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static java.awt.SystemColor.text;
+
+public class GeneralControlle {
+
+//    SlackClient slackClient;
+
+    @GetMapping(value = "test")
+    @PreAuthorize("hasRole('USER')")
+    public String test(){
+        return "${spring.profiles.active}";
+    }
+
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminOnly() {
+        return "You are an admin!";
+    }
+}
